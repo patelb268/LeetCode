08c2b496d4fe958dc6a0288c28900823a415a47c
PRAYAG PATEL <109325051+patelb268@users.noreply.github.com>
2023-09-11 15:33:21 -0700

    Update Solution.java

--- Problem 34/Solution.java
+++ Problem 34/Solution.java
@@ -1,32 +1,46 @@
 class Solution {
-    public int search(int[] nums, int target) {
+    public int[] searchRange(int[] nums, int target) {
+       
+        return new int [] { binary_search_left(nums,target),  binary_search_right(nums,target)};
+        
+    }
+
+    public int binary_search_left(int [] nums,int target){
         int low = 0;
         int high = nums.length - 1;
-        
+        int first = -1;
         while(low <= high){
-            int mid = low + (high - low)/2;
-            
-            if(nums[mid] == target) return mid;
-            
-            //find the sorted side
-            //left sorted
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target < nums[mid]){
-                    high = mid - 1;
-                }else{
-                    low = mid + 1;
-                }
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                first = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
             }else{
-                if(nums[mid] < target && target <= nums[high]){
-                    low = mid + 1;
-                }else{
-                    high = mid - 1;
-                }
-                
+                high= mid - 1;
             }
         }
-        return -1;
-        
+        return first;
+    }
+    
+    public int binary_search_right(int [] nums,int target){
+        int low = 0;
+        int high = nums.length - 1;
+        int last = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                last = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }else{
+                high= mid - 1;
+            }
+        }
+        return last;
     }
 }
 
