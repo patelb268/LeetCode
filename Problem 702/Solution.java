4bb2f3443f78f29d8ed6472762dcdbdf174df3cc
PRAYAG PATEL <109325051+patelb268@users.noreply.github.com>
2023-09-10 19:15:45 -0700

    Create Solution.java

---
+++ Problem 702/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface ArrayReader {
+ *     public int get(int index) {}
+ * }
+ */
+
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        int low = 0;
+        int high = 1;
+        while(target > reader.get(high)){
+            low = high;
+            high = high  * 2;
+        }
+        
+        while(low <= high){
+            int mid = low + (high -low) / 2;
+            if(reader.get(mid) == target) return mid;
+            if(reader.get(mid) < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+        
+    }
+}
+
+/**
+  * Time Complexity : O(log (n))
+  * Space Complexity : O(1)
