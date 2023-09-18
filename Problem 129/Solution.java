e456473cf2a4dda48de0f8bab527ccb612d09297
PRAYAG PATEL <109325051+patelb268@users.noreply.github.com>
2023-09-18 02:25:38 -0700

    Update Solution.java

--- Problem 129/Solution.java
+++ Problem 129/Solution.java
@@ -36,3 +36,68 @@ class Solution {
   * Time Complexity : O(N)
   * Space Complexity : O(H) H = height of tree
   */
+
+
+// Iterative Solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class Pair{
+        TreeNode node;
+        int num;
+        public Pair(TreeNode node, int num){
+            this.node = node;
+            this.num = num;
+        }
+        
+        public TreeNode getNode(){
+            return this.node;
+        }
+        
+        public int getNum(){
+            return this.num;
+        }
+    }
+    
+    public int sumNumbers(TreeNode root) {
+        Stack<Pair> st = new Stack<>();
+        int result = 0;
+        int currNum = 0;
+        while(root != null || !st.isEmpty()){
+            while(root != null){
+                currNum = currNum * 10 + root.val;
+                Pair pair = new Pair(root,currNum);
+                
+                st.push(pair);
+                root = root.left;
+            }
+            Pair currPair = st.pop();
+            currNum = currPair.getNum();
+            root = currPair.getNode();
+            if(root.left == null && root.right == null){
+                result += currNum;
+            }
+            root = root.right;
+        }
+        return result;
+    }
+}
+/**
+  * Time Complexity : O(N)
+  * Space Complexity : O(H) H = height of tree
+  */
+
